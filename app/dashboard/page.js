8af0b0091e81ed3d67556d7fcff41e6f20fae919
Vijay SB <vijaysb2006@gmail.com>
2024-12-02 15:28:13 +0530

    Added clerk Authentications

---
+++ app/dashboard/page.js
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+function dashboard() {
+  return (
+    <div>dashboard
+      <UserButton></UserButton>
+    </div>
+  )
+}
+
+export default dashboard
