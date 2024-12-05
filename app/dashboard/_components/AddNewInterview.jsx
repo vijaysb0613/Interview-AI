f09e95bfbec72850d34ab806d73ed469d49757d6
Vijay SB <vijaysb2006@gmail.com>
2024-12-06 00:21:34 +0530

    Form Created

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -1,48 +1,65 @@
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { Button } from '@/components/ui/button'
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input} from "@/components/ui/input"
+
+import { Button } from "@/components/ui/button";
 function AddNewInterview() {
-    const [OpenDialog,setOpenDialog] = useState(false)
+  const [OpenDialog, setOpenDialog] = useState(false);
   return (
     <div>
-    <div className='p-6 sm:p-8 border rounded-xl bg-gradient-to-r
+      <div
+        className="p-6 sm:p-8 border rounded-xl bg-gradient-to-r
      from-orange-400 to-orange-500 text-white shadow-md hover:scale-105 hover:shadow-lg
-      hover:from-orange-500 hover:to-orange-600 cursor-pointer transition-transform duration-300 ease-in-out'
-      onClick={()=>setOpenDialog(true)}
+      hover:from-orange-500 hover:to-orange-600 cursor-pointer transition-transform duration-300 ease-in-out"
+        onClick={() => setOpenDialog(true)}
       >
-      <h2 className='font-semibold text-lg text-center tracking-wide'>+ Add New</h2>
-   </div>
-   <Dialog open={OpenDialog}>
-  
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-        <div className='flex gap-5 justify-end'>
-            <Button variant="ghost" onClick={()=>setOpenDialog(false)}> 
+        <h2 className="font-semibold text-lg text-center tracking-wide">
+          + Add New
+        </h2>
+      </div>
+      <Dialog open={OpenDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <form action="">
+            <div>
+              <h2>Tell Us More About your Job </h2>
+              <h2>
+                Add Details about your job position/role,Job description, Years
+                of Experirence
+              </h2>
+              <div className="mt-7 my-2">
+                <label>Job Role/Jobe Positon</label>
+                <Input placeholder="EX. Full Stack Developer" required></Input>
+              </div>
+              <div className="mt-7 my-2">
+                <label>Job Description/Tech Stack (In Short)</label>
+                <Input placeholder="EX. React,Angular,Nodejs,MySQL" required></Input>
+              </div>
+              <div className="mt-7 my-2">
+                <label>Job Experience</label>
+                <Input placeholder="EX.5" type="number" max="50" required></Input>
+              </div>
+            </div>
+            <div className="flex gap-5 justify-end">
+              <Button type="button" variant="ghost" onClick={() => setOpenDialog(false)}>
                 Cancel
-            </Button>
-            <Button>
-                Start Inteview
-            </Button>
-        </div>
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
-
+              </Button>
+              <Button type="submit">Start Inteview</Button>
+            </div>
+            </form>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
 
-export default AddNewInterview
+export default AddNewInterview;
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,4 +1,4 @@
-import { UserButton } from '@clerk/nextjs'
+"use client"
 import React from 'react'
 import AddNewInterview from './_components/AddNewInterview'
 
