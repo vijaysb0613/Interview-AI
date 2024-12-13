256f24a415270408115d51e23ff803d6ff55ff35
Vijay SB <vijaysb2006@gmail.com>
2024-12-14 00:06:55 +0530

    Fetching Data from Db

--- app/dashboard/interview/[interviewId]/page.tsx
+++ app/dashboard/interview/[interviewId]/page.tsx
@@ -1,7 +1,21 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../../../utils/db'
+import { MockInterview } from '../../../../utils/schema'
+import { eq } from 'drizzle-orm'
 
 function Interview({params}) {
+  // const[interviewData,setInterviewData]=useState([]);
+  useEffect(()=>
+  {
+    GetInterviewData();
+  },[])
+  const GetInterviewData = async() =>
+  {
+    const result=await db.select().from(MockInterview).where(eq(MockInterview.mockId,params.interviewId))
+    console.log(result)
+    // setInterviewData(result[0])
+  }
   return (
     <div>Interview</div>
   )
