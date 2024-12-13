b812eff27f75a8c89a65b378decf17e5ded1f538
Vijay SB <vijaysb2006@gmail.com>
2024-12-14 00:46:20 +0530

    Webcam setup

--- app/dashboard/interview/[interviewId]/page.tsx
+++ app/dashboard/interview/[interviewId]/page.tsx
@@ -3,9 +3,12 @@ import React, { useEffect, useState } from 'react'
 import { db } from '../../../../utils/db'
 import { MockInterview } from '../../../../utils/schema'
 import { eq } from 'drizzle-orm'
+import Webcam from 'react-webcam'
+import { WebcamIcon } from 'lucide-react'
 
 function Interview({params}) {
-  // const[interviewData,setInterviewData]=useState([]);
+  const[interviewData,setInterviewData]=useState([]);
+  const[webCamEnabled,setWebCamEnabled]=useState(false);
   useEffect(()=>
   {
     GetInterviewData();
@@ -14,10 +17,22 @@ function Interview({params}) {
   {
     const result=await db.select().from(MockInterview).where(eq(MockInterview.mockId,params.interviewId))
     console.log(result)
-    // setInterviewData(result[0])
+    setInterviewData(result)
   }
   return (
-    <div>Interview</div>
+    <div className='my-10 flex justify-center flex-col items-center'>
+      <h2 className='font-bold text-2xl'>Lets Get Started</h2>
+      <div>
+        {webCamEnabled? <Webcam 
+        onUserMedia={()=>setWebCamEnabled(true)}
+        onUserMediaError={()=>setWebCamEnabled(false)}
+        style={{
+          height:300,
+          width:300
+        }}/>:
+        <WebcamIcon className='h-72 w-full my-7 p-20 bg-blue-200 rounded-md border'  />}
+      </div>
+    </div>
   )
 } 
 
