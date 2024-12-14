f6a37f67b9e851687df02cc71434cb1190e7e196
Vijay SB <vijaysb2006@gmail.com>
2024-12-14 19:25:49 +0530

    Styling for interview Page

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -4,8 +4,7 @@ import { db } from '../../../../utils/db'
 import { MockInterview } from '../../../../utils/schema'
 import { eq } from 'drizzle-orm'
 import Webcam from 'react-webcam'
-import { WebcamIcon } from 'lucide-react'
-
+import { Lightbulb, WebcamIcon } from 'lucide-react'
 import { Button } from '../../../../components/ui/button'
 import { useParams } from 'next/navigation'
 function Interview({params}) {
@@ -15,7 +14,8 @@ function Interview({params}) {
   {
     GetInterviewData();
   },[])
-  const interviewId=useParams()
+
+  const {interviewId}=useParams()
   const GetInterviewData = async() =>
   {
     const result=await db.select().from(MockInterview).where(eq(MockInterview.mockId,interviewId))
@@ -23,8 +23,27 @@ function Interview({params}) {
     setInterviewData(result)
   }
   return (
-    <div className='my-10 flex justify-center flex-col items-center'>
+    <div className='my-10 '>
       <h2 className='font-bold text-2xl'>Lets Get Started</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+      <div className='flex flex-col my-5 gap-10 '>
+        <div className='flex flex-col p-5 rounded-lg border'>
+        <h2 className='text-lg'><strong>Job Role/Position:</strong>{interviewData[0]?.jobPosition || "N/A"}</h2>
+        <h2 className='text-lg'><strong>Job Description</strong>{interviewData[0]?.jobDesc || "N/A"}</h2>
+        <h2 className='text-lg'><strong>Job Experience</strong>{interviewData[0]?.jobExperience || "N/A"}</h2>
+        </div>
+        <div className='p-5 rounded-md border-yellow-300 bg-yellow-600'>
+      <h2 className='flex gap-2 items-center'><Lightbulb /><strong>Information</strong></h2> 
+        <h2>The camera recording feature 
+          helps simulate real-world interviews 
+          by analyzing your body language, expressions,
+           and presentation skills to provide personalized feedback. Your recordings are securely stored and used
+            solely for feedback purposes, ensuring complete confidentiality. To get the best results, choose a quiet, well-lit space, position the camera at eye level,
+             and dress professionally as you would for an actual interview. Ensure that you have enabled webcam and microphone permissions for a
+           seamless recording experience.</h2>
+
+        </div>
+      </div>
       <div>
         {webCamEnabled? <Webcam 
         onUserMedia={()=>setWebCamEnabled(true)}
@@ -36,14 +55,18 @@ function Interview({params}) {
         mirrored={true}/>:
         <>
         <WebcamIcon className='h-72 w-full my-7 p-20 bg-blue-200 rounded-md border'  />
-        <Button onClick={()=>setWebCamEnabled(true)} variant="outline">Enable WebCam And Microphone</Button>
+        <Button onClick={()=>setWebCamEnabled(true)} variant="ghost">Enable WebCam And Microphone</Button>
         </>
 
 }
       </div>
-      <div>
-        <h2><strong>Job Role/Position:</strong>{interviewData.jobPosition}</h2>
+
       </div>
+      <div className='flex justify-end items-end'>
+      <Button>Start Interview</Button>
+      </div>
+     
+   
     </div>
   )
 } 
