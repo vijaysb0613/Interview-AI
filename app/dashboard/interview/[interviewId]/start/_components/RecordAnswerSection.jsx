964516cd55a33a2b980abc9df5e55c1e2493e88b
Vijay SB <vijaysb2006@gmail.com>
2025-03-07 23:10:53 +0530

    Add Speech to text hook

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -1,8 +1,23 @@
+"use client"
 import React from 'react'
 import Webcam from 'react-webcam'
 import Image from 'next/image'
 import { Button } from '../../../../../../components/ui/button'
+import useSpeechToText from 'react-hook-speech-to-text';
+
 function RecordAnswerSection() {
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false
+  });
+
   return (
     <div className='flex flex-col justify-center items-center'>
     <div className='flex flex-col  justify-center items-center bg-slate-100 rounded-lg p-4 my-12 flex-wrap'>
@@ -18,6 +33,16 @@ function RecordAnswerSection() {
          }} />  
         </div>
         <Button variant="outline">Start Recording</Button>
+        <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <ul>
+        {results.map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
         </div>
   )
 }
