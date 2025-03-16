e93379689490da74eb7334a318253a6de7fd3856
Vijay SB <vijaysb2006@gmail.com>
2025-03-16 19:44:11 +0530

    Recording user Answer in the database

--- app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/RecordAnswerSection.jsx
@@ -1,19 +1,23 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import Webcam from 'react-webcam'
-import Image from 'next/image'
-import { Button } from '../../../../../../components/ui/button'
-import useSpeechToText from 'react-hook-speech-to-text';
-import { Heading2, Mic } from 'lucide-react'
-import { toast } from 'sonner'
+"use client";
+import React, { useEffect, useState } from "react";
+import Webcam from "react-webcam";
+import Image from "next/image";
+import { Button } from "../../../../../../components/ui/button";
+import useSpeechToText from "react-hook-speech-to-text";
+import { Mic } from "lucide-react";
+import { toast } from "sonner";
 import { model } from "../../../../../../GeminiAIModal";
-import { useUser } from '@clerk/nextjs'
-import moment from 'moment'
-
-
-function RecordAnswerSection({activeQuestionIndex,MockInterviewQuestion,interviewData}) {
-  const {user} = useUser();
-  const [loading,setLoading]=useState();
+import { useUser } from "@clerk/nextjs";
+import moment from "moment";
+import { db } from "../../../../../../utils/db";
+import { UserAnser } from "../../../../../../utils/schema";
+function RecordAnswerSection({
+  activeQuestionIndex,
+  MockInterviewQuestion,
+  interviewData,
+}) {
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
   const generationConfig = {
     temperature: 1,
     topP: 0.95,
@@ -30,83 +34,101 @@ function RecordAnswerSection({activeQuestionIndex,MockInterviewQuestion,intervie
     stopSpeechToText,
   } = useSpeechToText({
     continuous: true,
-    useLegacyResults: false
+    useLegacyResults: false,
   });
-   const[userAnswer,setUserAnswer]= useState("")
-  const SvaeUserAnswer = async ()=>{
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-      ],
-    });
-    if(isRecording)
-    {
+  const [userAnswer, setUserAnswer] = useState("");
+
+  const StartStopRecording = () => {
+    if (isRecording) {
       setLoading(true);
       stopSpeechToText();
-      if(userAnswer?.length<10)
-      {
-        setLoading(false)
-        toast("Error While Saving please record again");
-      }
-      const FeedBackPrompt = "Question:"+MockInterviewQuestion[activeQuestionIndex]?.question+", UserAnser:"+userAnswer+"Depending on the given Question And User Answer give a rating for 10 and feedback of improvement in 3 to 5 lines in json format with rating field and feedback field";
-      const result = await chatSession.sendMessage(FeedBackPrompt);
-      const mockJsonResponse=(result.response.text()).replace('```json','').replace('```','');
-      console.log(mockJsonResponse); 
-      const JsonFeedbackResp=JSON.parse(mockJsonResponse);
-
-      const resp=await db.insert(userAnswer).values({
-
-        mockIdRef:interviewData?.mockId,
-        question:MockInterviewQuestion[activeQuestionIndex]?.question,
-        correctAns:MockInterviewQuestion[activeQuestionIndex]?.answer,
-        userAns:userAnswer,
-        feedback:JsonFeedbackResp?.feedback,
-        rating:JsonFeedbackResp?.rating,
-        userEmail:user?.primaryEmailAddress.emailAddress,
-        createdAt:moment().format('DD-MM-yyyy'),
-        
-      })
-      if(resp)
-      {
-        toast('User Answer Recorded Sucessfully')
-      }
-      setLoading(false);
-    }
-    else
-    {
+    } else {
       startSpeechToText();
     }
-  }
-  useEffect(()=>{
-results.map((result)=>{setUserAnswer(prevAns=>prevAns+result?.transcript)})
-  },[results])
+  };
+
+  useEffect(() => {
+    results.map((result) => {
+      setUserAnswer((prevAns) => prevAns + result?.transcript);
+    });
+  }, [results]);
+
+  useEffect(() => {
+    if (!isRecording && userAnswer.length > 10) {
+      UpdateUserAnswer();
+    }
+  }, [userAnswer]);
+
+  const UpdateUserAnswer = async () => {
+    setLoading(true);
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [],
+    });
+    console.log(userAnswer);
+    const FeedBackPrompt =
+      "Question:" +
+      MockInterviewQuestion[activeQuestionIndex]?.question +
+      ", UserAnser:" +
+      userAnswer +
+      "Depending on the given Question And User Answer give a rating for 10 and feedback of improvement in 3 to 5 lines in json format with rating field and feedback field";
+    const result = await chatSession.sendMessage(FeedBackPrompt);
+    const mockJsonResponse = result.response
+      .text()
+      .replace("```json", "")
+      .replace("```", "");
+    console.log(mockJsonResponse);
+    const JsonFeedbackResp = JSON.parse(mockJsonResponse);
+    console.log(interviewData.mockId)
+    const resp = await db.insert(UserAnser).values({
+      mockIdRef: interviewData?.mockId,
+      question: MockInterviewQuestion[activeQuestionIndex]?.question,
+      correctAns: MockInterviewQuestion[activeQuestionIndex]?.answer,
+      userAns: userAnswer,
+      feedback: JsonFeedbackResp?.feedback,
+      rating: JsonFeedbackResp?.rating,
+      userEmail: user?.primaryEmailAddress.emailAddress,
+      createdAt: moment().format("DD-MM-yyyy"),
+    });
+    if (resp) {
+      toast("User Answer Recorded Sucessfully");
+    }
+    setUserAnswer("");
+    setLoading(false);
+  };
 
   return (
-    <div className='flex flex-col justify-center items-center'>
-    <div className='flex flex-col  justify-center items-center bg-slate-100 rounded-lg p-4 my-12 flex-wrap'>
-        
-        <Image src="/webcam.jpg" width={200} height={200} alt="Webcam" className='absolute'  />
-        
-         <Webcam
-         mirrored={true}
-         style={{
-          height:300,
-          width:'100%',
-          zIndex:10,
-         }} />  
-        </div>
-        <Button variant="outline"
-        onClick={SvaeUserAnswer}>
-        {isRecording ?
-        <h2 className='text-red-600'>
-          <Mic /> Stop Recording
-        </h2>
-        :
-        'Record Answer'
-        }</Button>
-        <Button onClick={console.log(userAnswer)}>Show Answer</Button>
-        </div>
-  )
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col  justify-center items-center bg-slate-100 rounded-lg p-4 my-12 flex-wrap">
+        <Image
+          src="/webcam.jpg"
+          width={200}
+          height={200}
+          alt="Webcam"
+          className="absolute"
+        />
+
+        <Webcam
+          mirrored={true}
+          style={{
+            height: 300,
+            width: "100%",
+            zIndex: 10,
+          }}
+        />
+      </div>
+      <Button disabled={loading} variant="outline" onClick={StartStopRecording}>
+        {isRecording ? (
+          <h2 className="text-red-600">
+            <Mic /> Stop Recording
+          </h2>
+        ) : (
+          "Record Answer"
+        )}
+      </Button>
+      <Button onClick={console.log(userAnswer)}>Show Answer</Button>
+    </div>
+  );
 }
 
-export default RecordAnswerSection
+export default RecordAnswerSection;
--- app/dashboard/interview/[interviewId]/start/page.jsx
+++ app/dashboard/interview/[interviewId]/start/page.jsx
@@ -6,58 +6,81 @@ import { MockInterview } from "../../../../../utils/schema";
 import { eq } from "drizzle-orm";
 import QuestionSection from "./_components/QuestionSection";
 import RecordAnswerSection from "./_components/RecordAnswerSection";
+
 function StartInterview() {
   const [interviewData, setInterviewData] = useState(null);
   const [MockInterviewQuestion, setMockInterviewQuestion] = useState([]);
   const params = useParams();
   const interviewId = params?.interviewId;
-  const [activeQuestionIndex,setActiveQuestionIndex]=useState(0)
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    GetInterviewData();
-  }, []);
+    console.log("useEffect triggered, interviewId:", interviewId);
+    if (interviewId) {
+      GetInterviewData();
+    }
+  }, [interviewId]);
 
   const GetInterviewData = async () => {
     try {
+      setLoading(true);
+      console.log("Fetching interview data...");
+
       const result = await db
         .select()
         .from(MockInterview)
         .where(eq(MockInterview.mockId, interviewId));
 
+      console.log("Query result:", result);
+
       if (result.length > 0) {
         const jsonMockResp = JSON.parse(result[0].jsonMockResp);
-        console.log("Parsed Questions:", jsonMockResp);
-        const questionsArray = jsonMockResp.interviewQuestions || []; 
+        const questionsArray = jsonMockResp?.interviewQuestions || [];
+
+        setInterviewData(() => result[0]); // Functional state update
+        setMockInterviewQuestion(questionsArray);
+
+        console.log("Updated interviewData:", result[0]);
         console.log("Extracted Questions:", questionsArray);
-        setInterviewData(result[0]);
-        setMockInterviewQuestion(questionsArray); 
       } else {
         console.error("No interview data found");
       }
     } catch (error) {
       console.error("Error fetching interview data:", error);
+    } finally {
+      setLoading(false);
     }
-
   };
+
+  // Log interviewData when it updates
+  useEffect(() => {
+    console.log("interviewData after update:", interviewData);
+  }, [interviewData]);
 
   return (
     <div className="flex flex-row gap-5">
-    {/* Left Section - Questions */}
-    <div className="w-full md:w-1/2">
-      <QuestionSection 
-        activeQuestionIndex={activeQuestionIndex}
-        MockInterviewQuestion={MockInterviewQuestion} 
-      />
+      {/* Left Section - Questions */}
+      <div className="w-full md:w-1/2">
+        <QuestionSection
+          activeQuestionIndex={activeQuestionIndex}
+          MockInterviewQuestion={MockInterviewQuestion}
+        />
+      </div>
+
+      {/* Right Section - Answer Recording */}
+      <div className="w-full md:w-1/2">
+        {!loading && interviewData ? (
+          <RecordAnswerSection
+            activeQuestionIndex={activeQuestionIndex}
+            MockInterviewQuestion={MockInterviewQuestion}
+            interviewData={interviewData}
+          />
+        ) : (
+          <p>Loading interview data...</p>
+        )}
+      </div>
     </div>
-  
-    {/* Right Section - Answer Recording */}
-    <div className="w-full md:w-1/2">
-      <RecordAnswerSection
-        activeQuestionIndex={activeQuestionIndex}
-        MockInterviewQuestion={MockInterviewQuestion}
-        interviewData={interviewData} />
-    </div>
-  </div>
-  
   );
 }
 
